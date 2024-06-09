7044e4493b6bfeedbd05bc584e7cee7721605f7b
Deep-74 <deepghuge74@gmail.com>
2024-06-09 21:30:45 +0530

    Updates

---
+++ precache-manifest.945efd68cf8dd29db440a208f5c97a61.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b94aa0099620e1247918d645b0686bd",
+    "url": "/Algose/index.html"
+  },
+  {
+    "revision": "d53cfcc1d1b54fa5a856",
+    "url": "/Algose/static/css/main.0feff25c.chunk.css"
+  },
+  {
+    "revision": "6c36af8bf542921085fb",
+    "url": "/Algose/static/js/2.00de3bbc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Algose/static/js/2.00de3bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53cfcc1d1b54fa5a856",
+    "url": "/Algose/static/js/main.8f93ca4d.chunk.js"
+  },
+  {
+    "revision": "9128a0d452bac26f0996",
+    "url": "/Algose/static/js/runtime-main.bf7a6580.js"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/Algose/static/media/Anurati-Regular.ca8632f4.otf"
+  },
+  {
+    "revision": "bf570edf85c826128ac4fd5b4dcbba7f",
+    "url": "/Algose/static/media/analytics.bf570edf.svg"
+  },
+  {
+    "revision": "4c836d2b5448621906f6b5f9bb77bc8a",
+    "url": "/Algose/static/media/bar-chart.4c836d2b.svg"
+  },
+  {
+    "revision": "88bbe4eec45cd6c4d6e97cb2cdfbd50d",
+    "url": "/Algose/static/media/convex.88bbe4ee.svg"
+  },
+  {
+    "revision": "cfd6557ef662cce8ba82b98cda683394",
+    "url": "/Algose/static/media/convex_instruct.cfd6557e.gif"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/Algose/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "bd3c0250ddb00438c954b9f03dc36052",
+    "url": "/Algose/static/media/graph.bd3c0250.svg"
+  },
+  {
+    "revision": "eb2a63975cdaa68094fe7f8bed132ec3",
+    "url": "/Algose/static/media/heading_logo.eb2a6397.png"
+  },
+  {
+    "revision": "afc106ad260cdbb93ce12d296a167cc4",
+    "url": "/Algose/static/media/linkedin.afc106ad.svg"
+  },
+  {
+    "revision": "67aa2ccbada71528e9c6ccd05917cef8",
+    "url": "/Algose/static/media/pathfinder.67aa2ccb.gif"
+  },
+  {
+    "revision": "82c79bd880e304d8f008e48d81b794a4",
+    "url": "/Algose/static/media/pathfinding_front.82c79bd8.svg"
+  },
+  {
+    "revision": "2f3f3bdbd031928bf064c47c18e0ecca",
+    "url": "/Algose/static/media/planet.2f3f3bdb.svg"
+  },
+  {
+    "revision": "24f618090c99aa8e9fc203ebd46484e9",
+    "url": "/Algose/static/media/sorting.24f61809.png"
+  },
+  {
+    "revision": "4db25bca510cd839d849d71904990ba0",
+    "url": "/Algose/static/media/space-ship.4db25bca.svg"
+  }
+]);
